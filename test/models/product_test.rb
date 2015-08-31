510180109dc322318283f3bfc0c5dc1109409488
Joan Eikens <joan@hpenvy>
2015-08-31 11:47:48 -0600

    Finished chapter 10.

--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -1,6 +1,9 @@
 require 'test_helper'
 
 class ProductTest < ActiveSupport::TestCase
+  
+  fixtures :products
+  
   def new_product(image_url)
     Product.new(title:"My Book Title", description: "yyy",price: 1, image_url: image_url)
   end
@@ -39,5 +42,11 @@ class ProductTest < ActiveSupport::TestCase
     bad.each do |name|
       assert new_product(name).invalid?, "#{name} shouldn't be valid"
     end
+  end
+  
+  test "product is not valid without a unique title" do
+    product = Product.new(title: products(:ruby).title,description: "yyy",price: 1, image_url: "fred.gif" )
+    assert product.invalid?
+    assert_equal ["has already been taken"],product.errors[:title] 
   end
 end
